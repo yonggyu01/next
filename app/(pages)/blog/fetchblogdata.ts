c89d86b8e90414e5cb19109fcf71c2a82e41a90a
kimyong <yonggyu01@naver.com>
2024-05-18 09:26:24 +0900

    Fix: deploy버그 수정

---
+++ app/(pages)/blog/fetchblogdata.ts
@@ -0,0 +1,17 @@
+import {Steemitdata,SteemitResult} from "@/app/model"
+export const blogdata = async ():Promise<SteemitResult> => {
+  let data = await fetch('https://api.steemit.com', {
+   method: 'POST',
+   headers: {
+     'Content-Type': 'application/json',
+   },
+   body: JSON.stringify({
+     jsonrpc: '2.0',
+     method: 'condenser_api.get_blog',
+     params: ['yonggyu01', 0, 150],  
+     id: '1',}),
+  // cache: 'no-store'
+   })
+   let result = await data.json()
+   return result
+ }
--- app/(pages)/blog/page.tsx
+++ app/(pages)/blog/page.tsx
@@ -1,25 +1,9 @@
 import Image from "next/image";
 import React from 'react'
-
+import { blogdata } from "./fetchblogdata";
 import blog from './blog.module.scss'
 import { Bloglist } from "./bloglist";
 import {Steemitdata,SteemitResult} from "@/app/model"
-export const blogdata = async ():Promise<SteemitResult> => {
-  let data = await fetch('https://api.steemit.com', {
-   method: 'POST',
-   headers: {
-     'Content-Type': 'application/json',
-   },
-   body: JSON.stringify({
-     jsonrpc: '2.0',
-     method: 'condenser_api.get_blog',
-     params: ['yonggyu01', 0, 150],  
-     id: '1',}),
-  // cache: 'no-store'
-   })
-   let result = await data.json()
-   return result
- }
 
 export default async function Blog(){
   const data :SteemitResult = await blogdata()
