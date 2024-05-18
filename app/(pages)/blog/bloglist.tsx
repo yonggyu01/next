54615343d760524a0e26a9b5f41ee8f77cfdab7a
kimyong <yonggyu01@naver.com>
2024-05-18 12:31:16 +0900

    Fix: deploy버그 수정2

--- app/(pages)/blog/bloglist.tsx
+++ app/(pages)/blog/bloglist.tsx
@@ -14,49 +14,53 @@ export const Bloglist :React.FC<ownprops> = ({data}) => {
   const list  = useAppSelector(state => state.persistedReducer.mystore.blogdata)
   console.log(list)
   const [myreact,setreact] = React.useState<Steemitdata[]>([])
-  const [setdata,  setnewdata] = React.useState(new Set(myreact))
   const [myvue,setvue] =  React.useState<Steemitdata[]>([])
   const [myetc,setetc] =  React.useState<Steemitdata[]>([])
   const [mynext,setnext] =  React.useState<Steemitdata[]>([])
   const [myskill,setskill] =  React.useState<Steemitdata[]>([])
   const [mycs,setcs] =  React.useState<Steemitdata[]>([])
-  const [csdata,  setcsdata] = React.useState(new Set(mycs))
-  const [nextdata,  setnextdata] = React.useState(new Set(mycs))
-  const [skilldata,  setskilldata] = React.useState(new Set(mycs))
   
-  console.log(data.result)
+  console.log(myreact, '잘들어오나 보자')
     function filterHashtag(array:Steemitdata[]){
       array.forEach(item =>{
           let {tags} = JSON.parse(item.comment.json_metadata)  
           if (Array.isArray(tags)){
               for(let x of tags){
                 if(x === 'yongreact'){
-                  setreact(prevState => [...prevState, item]);
-                  setdata.add(item)
+                  setreact(prevState => {
+                    if(!prevState.includes(item)){return prevState = [...prevState, item]}
+                  return prevState}
+                  )
                   dispatch(Mystore.actions.addreact(item))
                   // setreact([...myreact, item])
                   // console.log(myreact, '일치',item)
                 }else if(x === 'yongvue'){
-                  setvue(prevState => [...prevState, item]);
-                    // setvue([...myvue,item])
+                  setvue(prevState => {
+                    if(!prevState.includes(item)){return prevState = [...prevState, item]}
+                  return prevState})
                     dispatch(Mystore.actions.addvue(item))
                 }else if(x === 'yongetc'){
                   //  setetc([...myetc,item])
-                  setetc(prevState => [...prevState, item]);
+                  setetc(prevState => {
+                    if(!prevState.includes(item)){return prevState = [...prevState, item]}
+                  return prevState})
                   dispatch(Mystore.actions.addetc(item))
                 }else if( x=== 'yongnext'){
-                  setnext(prevState => [...prevState, item]);
-                  nextdata.add(item)
+                  setnext(prevState => {
+                    if(!prevState.includes(item)){return prevState = [...prevState, item]}
+                  return prevState})
                   dispatch(Mystore.actions.addnext(item))
                   }else if( x=== 'yongcs'){
-                    setcs(prevState => [...prevState, item]);
-                    csdata.add(item)
+                    setcs(prevState => {
+                      if(!prevState.includes(item)){return prevState = [...prevState, item]}
+                    return prevState})
                     dispatch(Mystore.actions.addcs(item))
                     // console.log(mycs, 'cs 일치',item)
                    }else if( x=== 'yongskill'){
-                    setskill(prevState => [...prevState, item]);
+                    setskill(prevState => {
+                      if(!prevState.includes(item)){return prevState = [...prevState, item]}
+                    return prevState})
                     dispatch(Mystore.actions.addskill(item))
-                    skilldata.add(item)
                     // console.log(mycs, 'cs 일치',item)
                    }
               }
@@ -64,7 +68,6 @@ export const Bloglist :React.FC<ownprops> = ({data}) => {
         })
          
       }
-      console.log(setdata)
     useEffect(()=>{
       filterHashtag(data.result) 
     },[])
@@ -76,10 +79,10 @@ export const Bloglist :React.FC<ownprops> = ({data}) => {
           {/* <h1> 블로그입니다. </h1> */}
 
         <div className={bloglist.reactwrap}>
-        <Reactpage data={[...skilldata]} title='기술블로그' check='skill'></Reactpage>
-        <Reactpage data={[...setdata]} title='React' check="react"></Reactpage>
-        <Reactpage data={[...csdata]} title='Cs 기본지식' check='cs'></Reactpage>
-        <Reactpage data={[...nextdata]} title='next 기본지식' check='next'></Reactpage>
+        <Reactpage data={myskill} title='기술블로그' check='skill'></Reactpage>
+        <Reactpage data={myreact} title='React' check="react"></Reactpage>
+        <Reactpage data={mycs} title='Cs 기본지식' check='cs'></Reactpage>
+        <Reactpage data={mynext} title='next 기본지식' check='next'></Reactpage>
         </div>
 
 
