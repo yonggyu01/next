125fa0b6a0d710651d57e3ccfaa79142a3258b7e
kimyong <yonggyu01@naver.com>
2024-05-31 17:06:02 +0900

    Style: 환경변수 세팅
    api 키 전부 삭제 vercel에 환경변수 입력

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,17 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore ,collection, getDocs} from "firebase/firestore";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  // apiKey: "AIzaSyDlJ8F9V_cGZRwL_QLhfwlcJJI7BKmlFI8",
-  // authDomain: "next-todo-aaf48.firebaseapp.com",
-  // projectId: "next-todo-aaf48",
-  // storageBucket: "next-todo-aaf48.appspot.com",
-  // messagingSenderId: "62949706018",
-  // appId: "1:62949706018:web:48406ad6b36c5c80203d7b"
 
   apiKey: process.env.NEXT_PUBLIC_APIKEY,
   authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
