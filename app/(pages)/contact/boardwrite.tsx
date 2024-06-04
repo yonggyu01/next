f234bcdd634ace8c3dccaf17544bb5c5965c14ee
kimyong <yonggyu01@naver.com>
2024-06-04 12:47:51 +0900

    Feat: 방명록 모바일기능개선
    모바일화면에서 send버튼 추가 일부로직 변경

--- app/(pages)/contact/boardwrite.tsx
+++ app/(pages)/contact/boardwrite.tsx
@@ -8,13 +8,17 @@ addboard(obj : noticeboard) :void
 }
 const Boardwrite :React.FC<ownporps> =({addboard})=>{
 const inputid= React.useRef<HTMLInputElement>(null)
+const inputmoid= React.useRef<HTMLInputElement>(null)
 const inputtext= React.useRef<HTMLInputElement>(null)
+const inputtext1= React.useRef<HTMLInputElement>(null)
 const [userid, setid] = React.useState<string>() 
+const [moid, setmoid] = React.useState<string>() 
+const [usertext, settext] = React.useState<string>() 
 
 
     return (
     <div className={board.inputwrap}>
-        {!userid && <input type="text"  id='idinput' ref={inputid}  placeholder='ID입력하세요' onKeyDown={(e:React.KeyboardEvent<HTMLInputElement>)=>{
+        {!userid && <div className={board.flexinput}><input type="text" className={board.idinput} id='idinput' ref={inputid}  placeholder='ID입력하세요' onKeyDown={(e:React.KeyboardEvent<HTMLInputElement>)=>{
             console.log(e ,'id인풋')
             if(e.code === "Enter"){
                 let target =  e.target as HTMLInputElement
@@ -22,9 +26,24 @@ const [userid, setid] = React.useState<string>()
                 setid(target.value)
                 inputtext.current?.focus()
             }
-            }}/>}
-        {userid&& <input type="text" ref={inputtext} id='boardinput' placeholder='방명록을 입력하세요' onKeyDown={(e:React.KeyboardEvent<HTMLInputElement>)=>{
-           if(e.code ==="Enter"){
+            }}/>
+            <input type="text" className={board.moidinput} ref={inputmoid}  placeholder='ID입력하세요' value={moid} onChange={(e:React.ChangeEvent<HTMLInputElement>)=>{
+              let target =  e.target as HTMLInputElement
+                e.preventDefault()
+                setmoid(target.value)            
+            }
+            }/>
+              <button className={board.sendbtn} onClick={(e:React.MouseEvent<HTMLButtonElement,MouseEvent>)=>{
+         e.preventDefault();
+         if(moid==undefined){
+            alert('id를 입력해주세요')
+            return;}
+            setid(moid)
+        }}> send </button>
+            </div>}
+        {userid&& <div className={board.flexinput}>
+            <input className={board.webinput} type="text" ref={inputtext} id='boardinput' placeholder='방명록을 입력하세요' onKeyDown={(e:React.KeyboardEvent<HTMLInputElement>)=>{
+           if(e.code ==="Enter" || e.code ==="NumpadEnter"){
             let target = e.target as HTMLInputElement   
             e.preventDefault();
             const date :Date = new Date()
@@ -38,7 +57,28 @@ const [userid, setid] = React.useState<string>()
             target.value=''
             setid('')
            }
-        }}/>}
+        }} />
+        <input className={board.mobinput} type="text" ref={inputtext1} id='mobinput' placeholder='방명록을 입력하세요' value={usertext} onChange={(e: React.ChangeEvent<HTMLInputElement>)=>{
+            let target = e.target as HTMLInputElement   
+            e.preventDefault();
+            settext(target.value)//모바일환경을 위해 전송버튼 추가를 위해 넣음
+            console.log(usertext ,'입력이 잘 되고있는지')
+        }}/>
+         <button className={board.sendbtn} onClick={(e:React.MouseEvent<HTMLButtonElement,MouseEvent>)=>{
+         e.preventDefault();
+         if(usertext==undefined){
+            alert('내용을 입력해주세요')
+            return;}
+         const date :Date = new Date()
+         const createdate:string = (date.getMonth()+1) + '월' + date.getDate()+"일" + date.getHours() + "시" + date.getMinutes() +"분"
+         let data:noticeboard ={
+             content: usertext,
+             create : createdate,
+             userid 
+         }
+         addboard(data)
+        }}> send </button>
+        </div>}
     </div>
     )
 }
