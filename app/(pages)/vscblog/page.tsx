d2501ab572a7a14c7316f16e017ff581ac81186b
kimyong <yonggyu01@naver.com>
2024-06-15 10:04:09 +0900

    Fix: 배포시 오류 수정
    blog, contact  페이지 로직변경 props

--- app/(pages)/vscblog/page.tsx
+++ app/(pages)/vscblog/page.tsx
@@ -4,13 +4,31 @@ import { blogdata } from "../blog/fetchblogdata";
 import blog from './vscblog.module.scss'
 import { Bloglist } from "../blog/bloglist";
 import {Steemitdata,SteemitResult} from "@/app/model"
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+  DehydratedState,
+  useQuery,
+} from '@tanstack/react-query';
+async function preFetchdata() {
+  const queryClient = new QueryClient();
 
+  await queryClient.prefetchQuery({
+    queryKey: ['blogdatalist'],
+    queryFn: blogdata,
+  });
+  return  dehydrate(queryClient)  
+}
 export default async function Vscblog(){
-  const data :SteemitResult = await blogdata()
-  console.log(data)
+  const dehydratedState = await preFetchdata();
+  // const data :SteemitResult = await blogdata()
+  // console.log(data)
   return (
     <div className={blog.mainwrap}>
-     <Bloglist data={data}></Bloglist>
+       <HydrationBoundary state={dehydratedState}>
+     <Bloglist></Bloglist>
+     </HydrationBoundary>
     </div>
   );
 }
